03d47bca349e170f7c9288ef4a19850cb2144938
StephenDowling <125761746+StephenDowling@users.noreply.github.com>
2023-03-11 12:04:29 +0000

    First commit
    First commit for rock paper scissors game

---
+++ script.js
@@ -0,0 +1,39 @@
+/*
+    Rock Paper Scissors Game
+    S Dowling 21189994
+    11 03 2023
+*/
+
+function play(){
+    var userName;
+    var userChoice;
+    var computerChoice;
+    userName=(prompt("To begin, please enter your name"));
+    if(userName==""){
+        alert("Name cannot be blank. Please try again");
+    }
+    else{
+        userChoice=(prompt("Welcome "+userName+"! Please enter your choice - rock, paper or scissors"));
+        if(userChoice=="rock"){
+            computerChoice="paper";
+            alert("Computer has chosen "+computerChoice+"! Sorry "+userName+", you lose!")
+            hide();
+        }
+        else if(userChoice=="paper"){
+            computerChoice="scissors";
+            alert("Computer has chosen "+computerChoice+"! Sorry "+userName+", you lose!")
+            hide();
+        }
+        else if(userChoice=="scissors"){
+            computerChoice="rock"; 
+            alert("Computer has chosen "+computerChoice+"! Sorry "+userName+", you lose!")
+            hide();
+        }
+        else{
+            alert("Invalid input, please try again")
+        }
+    }
+}
+function hide() {
+    document.getElementById("game").style.display="none";
+}
